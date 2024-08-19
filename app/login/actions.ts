176b764987c77ef3d22ed8c1a8d944266c3cf2e9
Jason Aw <jason_aw1998@hotmail.com>
2024-08-19 12:03:13 +0800

    ko

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -5,26 +5,6 @@ import { redirect } from "next/navigation";
 
 import { createClient } from "@/utils/supabase/server";
 
-export async function login(formData: FormData) {
-  const supabase = createClient();
-
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
-  const data = {
-    email: formData.get("email") as string,
-    password: formData.get("password") as string,
-  };
-
-  const { error } = await supabase.auth.signInWithPassword(data);
-
-  if (error) {
-    redirect("/error");
-  }
-
-  revalidatePath("/", "layout");
-  redirect("/");
-}
-
 export async function signInWithEmail(formData: FormData) {
   const supabase = createClient();
 
