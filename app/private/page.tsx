de50d97a4651f231661072bf7be2e4a0a45aec67
Jason Aw <jason_aw1998@hotmail.com>
2024-08-19 11:58:10 +0800

    cookie

--- app/private/page.tsx
+++ app/private/page.tsx
@@ -1,9 +1,11 @@
 import { redirect } from "next/navigation";
 import { createClient } from "@/utils/supabase/server";
 import ChatArea from "@/components/ChatArea";
+import { cookies } from 'next/headers'
 
 const supabase = createClient();
 export default async function PrivatePage() {
+  const cookieStore = cookies()
   const { data, error } = await supabase.auth.getUser();
 
   if (error || !data?.user) {
